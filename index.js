6b89526b57c1d65c9bf785364eabdeb3be7f7b83
Ibrahim Naguib <80571858+Ibrahim-Naguib@users.noreply.github.com>
2024-08-20 04:16:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ require("dotenv").config();
 const app = express();
 const mongoose = require("mongoose");
 const { ERROR } = require("./utils/httpStatus");
-const { statusCode } = require("./utils/appError");
-const url = process.env.MONGO_URL;
+const url = process.env.MONGODB_URI;
 
 mongoose.connect(url).then(() => {
   console.log("Connected to the database");
