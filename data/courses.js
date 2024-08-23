c43e4d2bb4ff87f600c5b9a177069553da12802f
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-08-23 22:06:51 +0300

    /

--- data/courses.js
+++
@@ -1,7 +0,0 @@
-const courses = [
-  { id: 1, name: "HTML", price: 100 },
-  { id: 2, name: "CSS", price: 200 },
-  { id: 3, name: "JS", price: 300 },
-];
-
-module.exports = { courses };
--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-const express = require("express");
-const cors = require("cors");
-const coursesRouter = require("./routes/courses.route");
-const usersRouter = require("./routes/users.route");
-const path = require("path");
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const coursesRouter = require('./routes/courses.route');
+const usersRouter = require('./routes/users.route');
+const path = require('path');
+require('dotenv').config();
 
 const app = express();
-const mongoose = require("mongoose");
-const { ERROR } = require("./utils/httpStatus");
+const mongoose = require('mongoose');
+const { ERROR } = require('./utils/httpStatus');
 const url = process.env.MONGODB_URI;
 
 mongoose.connect(url).then(() => {
-  console.log("Connected to the database");
+  console.log('Connected to the database');
 });
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/courses", coursesRouter);
-app.use("/api/users", usersRouter);
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use('/api/courses', coursesRouter);
+app.use('/api/users', usersRouter);
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.all("*", (req, res, next) => {
-  return res.status(404).json({ status: ERROR, message: "Resource not found" });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'data', 'docs.json'));
+});
+
+app.all('*', (req, res, next) => {
+  return res.status(404).json({ status: ERROR, message: 'Resource not found' });
 });
 
 app.use((error, req, res, next) => {
@@ -34,5 +38,5 @@ app.use((error, req, res, next) => {
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is running on port 3000");
+  console.log('Server is running on port 3000');
 });
